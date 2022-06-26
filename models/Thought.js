bc27dd730c2aab1591b04830880f0ac3647b4f62
Travis Bates <tbateswsu@gmail.com>
2022-06-26 16:48:50 -0700

    feat: added the thoughtSchema, moment.js, and getter to format the datetimestamp with moment.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,26 @@
 const mongoose = require("mongoose");
+const moment = require("moment");
 
 // Define a new schema named `thoughtSchema`
-const thoughtSchema = new mongoose.Schema({});
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtTimeStamp) =>
+      moment(createdAtTimeStamp).format("MMM DD, YYYY [at] hh:mm a"),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [ReactionsSchema],
+});
 
 module.exports = Thought;
 
