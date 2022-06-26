b6d558d1b76a7b22c032c5bd69ceab27c04ecb1d
Travis Bates <tbateswsu@gmail.com>
2022-06-26 16:39:08 -0700

    Feat: Added the User Schema and virtual friendcount function that retrieves the user's friend array length.

--- models/Thought.js
+++ models/Thought.js
@@ -3,12 +3,6 @@ const mongoose = require("mongoose");
 // Define a new schema named `thoughtSchema`
 const thoughtSchema = new mongoose.Schema({});
 
-// Add the subdocuments to the parent document
-
-// Create a model named `Thought`
-
-// Create a new instance of the model
-
 module.exports = Thought;
 
 // Thought:
--- models/User.js
+++ models/User.js
@@ -1,12 +1,69 @@
 const mongoose = require("mongoose");
+// Validate email
+var validateEmail = (email) => {
+  let emailRegex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  return emailRegex.test(email);
+};
 
 // Define a new schema named `userSchema`
-const userSchema = new mongoose.Schema({});
+const UsersSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validateEmail, "Please fill in a valid email address!"],
+    match: [
+      /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+      "Please fill in a valid email address!",
+    ],
+  },
+  thoughts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thoughts",
+    },
+  ],
+  friends: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Users",
+    },
+  ],
+});
 
-// Add the subdocuments to the parent document
+UsersSchema.virtual("friendCount").get(() => {
+  return this.friends.length;
+});
 
-// Create a model named `User`
+const Users = model("Users", UsersSchema);
 
-// Create a new instance of the model
+module.exports = Users;
 
-module.exports = User;
+// User:
+
+// username
+//// String
+//// Unique
+//// Required
+//// Trimmed
+
+// email
+//// String
+//// Required
+//// Unique
+//// Must match a valid email address (look into Mongoose's matching validation)
+
+// thoughts
+//// Array of _id values referencing the Thought model
+
+// friends
+//// Array of _id values referencing the User model (self-reference)
+
+// Schema Settings:
+//// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
