57623768e957fc3311900506184939c90603d8f6
Travis Levi B <tbateswsu@gmail.com>
2022-06-28 13:46:55 -0700

    feat: added virtual to Thoughts model that gets the reactions length and returns it

--- models/Thought.js
+++ models/Thought.js
@@ -25,24 +25,37 @@ const reactionSchema = new Schema({
 });
 
 // Define a new schema named `thoughtSchema`
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtTimeStamp) =>
+        moment(createdAtTimeStamp).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtTimeStamp) =>
-      moment(createdAtTimeStamp).format("MMM DD, YYYY [at] hh:mm a"),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema],
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+thoughtSchema.virtual("reactionCount").get(() => {
+  return this.reactions.length;
 });
 
 const Thought = model("thought", thoughtSchema);
