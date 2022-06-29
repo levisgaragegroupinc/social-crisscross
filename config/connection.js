e6e969118936d86e8fda85a8234be03f9cf3f5a1
Travis Bates <tbateswsu@gmail.com>
2022-06-29 15:54:36 -0700

    debug: config file

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
 const { connect, connection } = require("mongoose");
 
 const connectionString =
+  // process.env.MONGODB_URI || "mongodb://localhost/usersDB";
   process.env.MONGODB_URI || "mongodb://localhost:27017/usersDB";
+// process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/usersDB";
+// process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/usersDB";
 
 connect(connectionString, {
   useNewUrlParser: true,
